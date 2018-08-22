00a88fa1f070289d06ebe3d05cd7d68f1da9fa31
Kyle <lookerkj@protonmail.com>
2018-08-22 09:18:04 -0700

    added a config file that is passing test

---
+++ config.js
@@ -0,0 +1,8 @@
+'use strict';
+// ========================================================
+// Exporting to mLab database
+// ========================================================
+
+module.exports = {
+  'CLIENT_ORIGIN' : 'https://obscure-retreat-46503.herokuapp.com/home',
+};
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
  const express = require('express');
  const app = express();
-//  const cors = require('cors');
-//  const {CLIENT_ORIGIN} = require('./config');
+ const cors = require('cors');
+ const {CLIENT_ORIGIN} = require('./config');
 
-// app.use(
-//     cors({
-//         origin: CLIENT_ORIGIN
-//     })
-// );
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
  const PORT = process.env.PORT || 3000;
 
