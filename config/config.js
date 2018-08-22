a0ea8e57e9146e87f4fe36207a1959a4d9e48a62
Kyle <lookerkj@protonmail.com>
2018-08-22 11:09:18 -0700

    need a break

---
+++ config/config.js
@@ -0,0 +1,8 @@
+'use strict';
+// ========================================================
+// Exporting client origin
+// ========================================================
+
+module.exports = {
+  'CLIENT_ORIGIN' : 'https://obscure-retreat-46503.herokuapp.com/home',
+};
