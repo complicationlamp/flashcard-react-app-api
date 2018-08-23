da9c358021296e7f344ae7e9f8692cd09cb4377d
Kyle <lookerkj@protonmail.com>
2018-08-22 17:58:13 -0700

    added path to mlab collection flashcard to config file

--- config/config.js
+++ config/config.js
@@ -4,5 +4,8 @@
 // ========================================================
 
 module.exports = {
+  // Exporting client origin
   'CLIENT_ORIGIN' : 'https://obscure-retreat-46503.herokuapp.com/home',
+  // Exporting to the mLab db collection flashcard-react-app
+  'URL': 'mongodb://complicationlamp:gradschool1@ds227352.mlab.com:27352/flashcard-react-app'
 };
