f2f306e689bad6618f3f9d0303deb9272bdeba28
Kyle <lookerkj@protonmail.com>
2018-08-22 18:09:18 -0700

    missed the DB_URL is in another file

--- config/database.js
+++ config/database.js
@@ -6,5 +6,4 @@
 
 module.exports = {
 	'DB_URL' : 'mongodb://complicationlamp:gradschool1@ds227352.mlab.com:27352/flashcard-react-app',
-	// 'test' : 'mongodb://complicationlamp:gradschool1@ds139920.mlab.com:39920/test-users-db'
   };
